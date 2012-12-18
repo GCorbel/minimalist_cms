0d2d0ba07cacc3fb08574d798bedac438e203555
gcorbel <guirec.corbel@gmail.com>
2012-12-18 06:28:21 -0500

    add globalize3

--- spec/dummy/db/migrate/20121216194550_create_pages.rb
+++ spec/dummy/db/migrate/20121216194550_create_pages.rb
@@ -1,11 +1,14 @@
 class CreatePages < ActiveRecord::Migration
-  def change
+  def up
     create_table :pages do |t|
-      t.string :title
-      t.text :body
       t.string :path
-
       t.timestamps
     end
+    Page.create_translation_table! title: :string, body: :text
+  end
+
+  def down
+    drop_table :pages
+    Page.drop_translation_table!
   end
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -14,8 +14,6 @@
 ActiveRecord::Schema.define(:version => 20121216194550) do
 
   create_table "pages", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
     t.string   "path"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
