79cb87e155fdbb01ba3cc6692917c3c46c0c2001
gcorbel <guirec.corbel@gmail.com>
2012-12-19 19:20:34 -0500

    remove paths column from database

--- spec/dummy/db/migrate/20121216194550_create_pages.rb
+++ spec/dummy/db/migrate/20121216194550_create_pages.rb
@@ -1,7 +1,6 @@
 class CreatePages < ActiveRecord::Migration
   def up
     create_table :pages do |t|
-      t.string :path
       t.timestamps
     end
     Page.create_translation_table! title: :string, body: :text, slug: :string
