f97aa9e103ba4517692441d89adbe72f9ad26329
gcorbel <guirec.corbel@gmail.com>
2012-12-19 19:16:44 -0500

    copy migration into engine

---
+++ db/migrate/20121216194550_create_pages.rb
@@ -0,0 +1,14 @@
+class CreatePages < ActiveRecord::Migration
+  def up
+    create_table :pages do |t|
+      t.string :path
+      t.timestamps
+    end
+    Page.create_translation_table! title: :string, body: :text, slug: :string
+  end
+
+  def down
+    drop_table :pages
+    Page.drop_translation_table!
+  end
+end
