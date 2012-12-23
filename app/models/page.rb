d65dcd57458c62dc1f758d92dcf6100f02d2af9b
gcorbel <guirec.corbel@gmail.com>
2012-12-23 06:51:20 -0500

    remove unused attributes

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   extend FriendlyId
 
-  attr_accessible :title, :body, :path, :path
+  attr_accessible :title, :body
   translates :title, :body, :slug, :meta_keywords, :meta_description
 
   Globalize::ActiveRecord::Translation.class_eval do
