9eb94a70132074714a4c58c0977bc335e155c9ff
gcorbel <guirec.corbel@gmail.com>
2012-12-22 20:30:35 -0500

    raise an error when the page is not found

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def show
     @page = Page.find_by_slug(params[:id])
+    raise ActionController::RoutingError.new('Page Not Found') if @page.nil?
   end
 end
--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+
+  let(:page) { stub_model(Page) }
+
   describe :show do
-    it 'should find by slug' do
-      Page.should_receive(:find_by_slug).with('test')
-      get :show, id: 'test'
+    context "when there is a page" do
+      it 'should find by slug' do
+        Page.should_receive(:find_by_slug).with('test').and_return(page)
+        get :show, id: 'test'
+      end
+    end
+
+    context "when there is no page" do
+      it 'raise an exception' do
+        lambda {
+          get :show, id: 'test'
+        }.should raise_error(ActionController::RoutingError)
+      end
     end
   end
 end
